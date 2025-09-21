fe6db202bf60411109e7d94380fa4f7bf6cbe22a
Chris Nielson <ctniel1@gmail.com>
2025-09-21 15:00:16 -0600

    Add tic tac toe to project screen

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,16 @@ export default function ProjectsPage() {
     <main className="min-h-screen p-6">
       <h1 className="mb-4 text-3xl font-bold">Projects</h1>
       <p>{`Here you'll find a selection of projects I've worked on. More coming soon!`}</p>
+      {/* Add your project listings here */}
+      <div>
+        <a
+          href="https://tic-tac-toe-cn.vercel.app/"
+          target="_blank"
+          className="block border-b border-gray-200 p-4"
+        >
+          Tic-Tac-Toe
+        </a>
+      </div>
     </main>
   );
 }
